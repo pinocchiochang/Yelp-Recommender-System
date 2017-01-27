8c6a608d7e0e628450191bff04b8a801ac75640d
Yuchang Chen <yc1181@nyu.edu>
2017-01-27 15:33:54 -0500

    Update review.java

--- preprocess/reviews/review.java
+++ preprocess/reviews/review.java
@@ -5,21 +5,21 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-public class Review {
-public static void main(String[] args) throws Exception {
-if (args.length != 2) {
-System.err.println("Usage: Count <input path> <output path>");
-System.exit(-1);
-}
-Job job = new Job();
-job.setJarByClass(Review.class);
-job.setJobName("Business statistics");
-FileInputFormat.addInputPath(job, new Path(args[0]));
-FileOutputFormat.setOutputPath(job, new Path(args[1]));
-job.setMapperClass(ReviewMapper.class);
-job.setReducerClass(ReviewReducer.class);
-job.setOutputKeyClass(Text.class);
-job.setOutputValueClass(IntWritable.class);
-System.exit(job.waitForCompletion(true) ? 0 : 1);
-}
+public class review {
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: Count <input path> <output path>");
+      System.exit(-1);
+    }
+    Job job = new Job();
+    job.setJarByClass(Review.class);
+    job.setJobName("Business statistics");
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setMapperClass(ReviewMapper.class);
+    job.setReducerClass(ReviewReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
